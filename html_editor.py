02f96053cda87620f53f47296d3498489c60cdd6
Ahmet Firat Gamsiz <ahmetfiratgamsiz287@gmail.com>
2022-08-31 13:04:58 +0300

    added maps to page

--- html_editor.py
+++ html_editor.py
@@ -1,5 +1,7 @@
 import bs4
 import sqlite3
+from geopy.geocoders import Nominatim
+import folium
 
 
 def query(cities, db):
@@ -14,7 +16,7 @@ def query(cities, db):
         ratings = [rating for rating in cur.execute("SELECT rating FROM products WHERE city_name = '{}'".format(city))]
         reviews = [review for review in cur.execute("SELECT reviews FROM products WHERE city_name = '{}'".format(city))]
         abouts = [about for about in cur.execute("SELECT about FROM products WHERE city_name = '{}'".format(city))]
-        city_table = {'names': names, 'photos': photos, 'ratings': ratings, 'reviews': reviews,'abouts':abouts}
+        city_table = {'names': names, 'photos': photos, 'ratings': ratings, 'reviews': reviews, 'abouts': abouts}
         cities_table.update({city: city_table})
     con.close()
     return cities_table
@@ -31,14 +33,24 @@ def create_guide(cities, db, pass_name, flight_list):
     pass_name_h2 = soup.find(id='intro').find('h2')
     pass_name_h2.append(pass_name + '!')
 
+    geolocator = Nominatim(user_agent='admin')
+
     for i in range(len(cities)):
         city = cities[i]
+
+        city_loc = geolocator.geocode(city).point
+        m = folium.Map(location=[city_loc.latitude, city_loc.longitude], tiles='openstreetmap', zoom_start=10)
         city_data = cities_table[city]
         city_name_h2 = soup.find(id='city-{}'.format(i)).find('h2', {'class': 'city-name'})
-        city_name_h2.append(city.capitalize())
+        city_name_h2.append(city.title())
 
         str_template = ''
         for j in range(len(city_data['names'])):
+            try:
+                point = geolocator.geocode(city_data['names'][j][0]).point
+                folium.Marker([point.latitude, point.longitude], popup=city_data['names'][j][0]).add_to(m)
+            except:
+                pass
             spot_template = ''
             if j % 2 == 0:
                 spot_template += '''<div class="text-on-left"><div class="left-text"><h3 class="dest-name">{}
@@ -54,9 +66,13 @@ def create_guide(cities, db, pass_name, flight_list):
             for review in review_list:
                 if len(review) > 500:
                     hashed = str(abs(hash(review)))
-                    script.write( '''function readMore{0}(){{let dots = document.getElementById('dots-{0}');let more = document.getElementById('more-{0}');let btnText = document.getElementById('more-btn-{0}');'''.format(hashed)+\
-                    '''if(dots.style.display ==='none'){dots.style.display ='inline';btnText.innerHTML = 'Read more';more.style.display='none';} else{dots.style.display = 'none';btnText.innerHTML = 'Read less';more.style.display ='inline';}}\n''')
-                    review = review[:500] + "<span id='dots-{0}' style='display:inline'>...</span><span id='more-{0}' ".format(hashed) + "style='display:none'>" + review[500:] + "</span>" + \
+                    script.write(
+                        '''function readMore{0}(){{let dots = document.getElementById('dots-{0}');let more = document.getElementById('more-{0}');let btnText = document.getElementById('more-btn-{0}');'''.format(
+                            hashed) + \
+                        '''if(dots.style.display ==='none'){dots.style.display ='inline';btnText.innerHTML = 'Read more';more.style.display='none';} else{dots.style.display = 'none';btnText.innerHTML = 'Read less';more.style.display ='inline';}}\n''')
+                    review = review[
+                             :500] + "<span id='dots-{0}' style='display:inline'>...</span><span id='more-{0}' ".format(
+                        hashed) + "style='display:none'>" + review[500:] + "</span>" + \
                              '''<button id="more-btn-{0}" onclick="readMore{0}()">Read more</button>'''.format(hashed)
                 spot_template += "<p> <span class='inline'> {} </span> </p>".format(review)
 
@@ -70,7 +86,8 @@ def create_guide(cities, db, pass_name, flight_list):
                 spot_template += '''</div><div class='break'''
                 if j % 2 == 1:
                     spot_template += ''' gray'''
-                spot_template += ''''><span class='about-h'>About</span><br> <span class='about'>{}</span>'''.format(city_data['abouts'][j][0])
+                spot_template += ''''><span class='about-h'>About</span><br> <span class='about'>{}</span>'''.format(
+                    city_data['abouts'][j][0])
             spot_template += '</div>'
             str_template += spot_template
 
@@ -80,10 +97,13 @@ def create_guide(cities, db, pass_name, flight_list):
                 flight_template += '<div class="flight-row'
                 if i % 2 == 1:
                     flight_template += ' gray'
-                flight_template += '"><span class="f_info">Departure: {}<br>Arrival: {}</span><a class="f_code" href="https://www.google.com/search?q={}">Flight code: {}</a></div>'.format(flight[0], flight[1], flight[2], flight[2])
+                flight_template += '"><span class="f_info">Departure: {}<br>Arrival: {}</span><a class="f_code" href="https://www.google.com/search?q={}">Flight code: {}</a></div>'.format(
+                    flight[0], flight[1], flight[2], flight[2])
             flight_template += '</div>'
             str_template += flight_template
 
+        m.save(f'site_template/maps/{city}.html')
+        str_template = f'<iframe id="{city}" src="maps/{city}.html" width=60vw height=40vh></iframe>' + str_template
         template = bs4.BeautifulSoup(str_template, features='html.parser')
         city_name_h2.insert_after(template)
     with open(f"site_template\\{pass_name}.html", "w", encoding='utf-8') as outf:
